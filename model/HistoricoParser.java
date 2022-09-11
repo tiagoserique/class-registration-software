0bca94ba339313b7a423fe79e17a84e2445ed8af
Luan-MB <luanmb@gmail.com>
2022-09-11 20:46:05 -0300

    Arruma crash ao tentar parsear linha ;;;;;;;;;;;;;;

--- model/HistoricoParser.java
+++ model/HistoricoParser.java
@@ -12,6 +12,17 @@ public class HistoricoParser {
         this.historicFile = historicFile;
     }
 
+    private String[] tryToSplit (String line) {
+        try {
+            String[] fields = line.split(";");
+            return fields;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            String[] fields = {};
+            return fields;
+        }
+    }
+
     public Vector<Vector<MateriaHistorico>> parseHistorico() {
         
         CsvReader reader = new CsvReader(this.historicFile);
@@ -22,33 +33,35 @@ public class HistoricoParser {
 
         for (String line: lines) {
 
-            String[] fields = line.split(";");
+            String[] fields = tryToSplit(line);
+            if (fields.length > 0) {
 
-            MateriaHistorico materiaHistorico = new MateriaHistorico();
+                MateriaHistorico materiaHistorico = new MateriaHistorico();
 
-            materiaHistorico.setMatrAluno(fields[0]);
-            materiaHistorico.setNomePessoa(fields[1]);
-            materiaHistorico.setCodCurso(fields[2]);
-            materiaHistorico.setNomeCurso(fields[3]);
-            materiaHistorico.setNumVersao(fields[4]);
-            materiaHistorico.setAno(fields[5]);
-            materiaHistorico.setMediaFinal(fields[6]);
-            materiaHistorico.setSituacaoItem(fields[7]);
-            materiaHistorico.setPeriodo(fields[8]);
-            materiaHistorico.setSituacao(fields[9]);
-            materiaHistorico.setCodAtivCurric(fields[10]);
-            materiaHistorico.setNomeAtivCurric(fields[11]);
-            materiaHistorico.setChTotal(fields[12]);
-            materiaHistorico.setDescrEstrutura(fields[13]);
-            materiaHistorico.setFrequencia(fields[14]);
-            materiaHistorico.setSigla(fields[15]);
-        
-            historico.add(materiaHistorico);
+                materiaHistorico.setMatrAluno(fields[0]);
+                materiaHistorico.setNomePessoa(fields[1]);
+                materiaHistorico.setCodCurso(fields[2]);
+                materiaHistorico.setNomeCurso(fields[3]);
+                materiaHistorico.setNumVersao(fields[4]);
+                materiaHistorico.setAno(fields[5]);
+                materiaHistorico.setMediaFinal(fields[6]);
+                materiaHistorico.setSituacaoItem(fields[7]);
+                materiaHistorico.setPeriodo(fields[8]);
+                materiaHistorico.setSituacao(fields[9]);
+                materiaHistorico.setCodAtivCurric(fields[10]);
+                materiaHistorico.setNomeAtivCurric(fields[11]);
+                materiaHistorico.setChTotal(fields[12]);
+                materiaHistorico.setDescrEstrutura(fields[13]);
+                materiaHistorico.setFrequencia(fields[14]);
+                materiaHistorico.setSigla(fields[15]);
+            
+                historico.add(materiaHistorico);
 
-            String periodoAno = materiaHistorico.getPeriodo() + materiaHistorico.getAno();
+                String periodoAno = materiaHistorico.getPeriodo() + materiaHistorico.getAno();
 
-            if (!semestres.contains(periodoAno)) {
-                semestres.add(periodoAno);
+                if (!semestres.contains(periodoAno)) {
+                    semestres.add(periodoAno);
+                }
             }
         }
 
